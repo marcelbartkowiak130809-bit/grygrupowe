9d04df1c4cc44633caa6624d7cbdb811ee799653
marcelbartkowiak130809-bit <marcelbartkowiak130809@gmail.com>
2026-08-04 01:46:14 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ import { createClosestTruthGame, ClosestTruthEngine, sanitizeClosestTruthSetting
 import { createRankingGame, RankingEngine, sanitizeRankingSettings } from "./ranking.js?v=20260612-2";
 import { createFiveSecondsGame, FiveSecondsEngine, sanitizeFiveSecondsSettings, stopFiveSecondsTimer } from "./fiveSeconds.js?v=20260612-2";
 import { createClockGame, ClockEngine, sanitizeClockSettings, stopClockTimer } from "./clock.js?v=20260613-1";
-import { createPokemonGame, PokemonEngine, stopPokemonTimer } from "./pokemon.js?v=20260804-5";
+import { createPokemonGame, PokemonEngine, stopPokemonTimer } from "./pokemon.js?v=20260804-6";
 import { createRoomModal, renderLobby } from "./lobby.js?v=20260804-2";
 import { renderPlatform } from "./platform.js?v=20260804-3";
 import { activatePublicAds, adSenseBlock, deactivatePublicAds, renderPublicPage } from "./publicPages.js?v=20260613-1";
