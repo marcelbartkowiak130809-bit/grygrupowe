0082985beb6e41febfc3650f024c631848a16c5d
marcelbartkowiak130809-bit <marcelbartkowiak130809@gmail.com>
2026-08-22 02:55:37 +0200

    Popraw wyglad okna statystyk strony

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ import { createSequenceGame, SequenceEngine, markSequenceReady, timeoutSequenceC
 import { createFamilyGame, FamilyEngine, stopFamilyTimer } from "./family.js?v=20260805-1";
 import { createWordChainGame, WordChainEngine, stopWordChainTimer } from "./wordChain.js?v=20260813-1";
 import { createRoomModal, renderLobby } from "./lobby.js?v=20260804-6";
-import { renderPlatform, renderPokemonModes } from "./platform.js?v=20260804-10";
+import { renderPlatform, renderPokemonModes } from "./platform.js?v=20260822-1";
 import { activatePublicAds, adSenseBlock, deactivatePublicAds, renderPublicPage } from "./publicPages.js?v=20260613-1";
 import { Router } from "./router.js";
 import { playerMini, renderRoom } from "./room.js?v=20260804-5";
