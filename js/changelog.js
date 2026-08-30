977969281e8da552d49506f34a340b4595d240fc
marcelbartkowiak130809-bit <marcelbartkowiak130809@gmail.com>
2026-08-30 19:59:44 +0200

    Temporarily unlock new multiplayer modes

--- js/changelog.js
+++ js/changelog.js
@@ -1,4 +1,4 @@
-import { modeUnlockInfo } from "./upcomingModes.js?v=20260830-1";
+import { modeUnlockInfo } from "./upcomingModes.js?v=20260830-2";
 
 const stagedModes = [
   { id: "bomba", name: "Bomba" },
